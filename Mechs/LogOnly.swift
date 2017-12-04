20ced6b952fd3cc9ca7cf52523890088efe45047
Josh Wisenbaker <josh@orchardandgrove.com>
2017-12-04 14:50:02 -0500

    cleanup in LogOnly.swift

--- Mechs/LogOnly.swift
+++ Mechs/LogOnly.swift
@@ -10,67 +10,62 @@ import Foundation
 import Security.AuthorizationTags
 import SecurityInterface.SFAuthorizationPluginView
 
+/// AuthorizationPlugin mechanism that simply logs the hint and context values that are being passed around.
 class LogOnly : NoLoMechanism {
     
-    let contextKeys = [ kAuthorizationEnvironmentUsername, kAuthorizationEnvironmentPassword, kAuthorizationEnvironmentShared, kAuthorizationRightExecute, kAuthorizationEnvironmentIcon, kAuthorizationEnvironmentPrompt ]
+    let contextKeys = [kAuthorizationEnvironmentUsername, kAuthorizationEnvironmentPassword, kAuthorizationEnvironmentShared, kAuthorizationRightExecute, kAuthorizationEnvironmentIcon, kAuthorizationEnvironmentPrompt]
     
     let hintKeys = [
-    "uid",
-    "gid",
-    "longname",
-    "shell",
-    "authorize-right",
-    "authorize-rule",
-    "client-path",
-    "client-pid",
-    "client-type",
-    "client-uid",
-    "client-pid",
-    "tries",
-    "suggested-user",
-    "require-user-in-group",
-    "reason",
-    "token-name",
-    "afp_dir",
-    "kerberos-principal",
-    "mountpoint",
-    "new-password",
-    "show-add-to-keychain",
-    "add-to-keuychain",
-    "Home_Dir_Mount_Result",
-    "homeDirType",
-    ]
+        "uid",
+        "gid",
+        "longname",
+        "shell",
+        "authorize-right",
+        "authorize-rule",
+        "client-path",
+        "client-pid",
+        "client-type",
+        "client-uid",
+        "client-pid",
+        "tries",
+        "suggested-user",
+        "require-user-in-group",
+        "reason",
+        "token-name",
+        "afp_dir",
+        "kerberos-principal",
+        "mountpoint",
+        "new-password",
+        "show-add-to-keychain",
+        "add-to-keuychain",
+        "Home_Dir_Mount_Result",
+        "homeDirType",
+        ]
     
     // class to iterate anything in the context and hits and print them out
     // heavily influenced by the Apple NullAuth sample code
     
     @objc func run() {
-        
         // starting with the context basics
-        
-        NSLog("User: \(self.username), Pass: \(self.password)")
-        NSLog("UID: \(self.uid)")
-        
+        myLogger.logit(.info, message: "User logging in: \(String(describing: username))")
+        myLogger.logit(.info, message: "UID of user logging in: \(String(describing: uid))")
+
         self.getArguments()
         self.getTokens()
         
         for item in contextKeys {
             let result = getContext(contextType: item)
             if result != nil {
-                NSLog("Context Item \(item): \(result)")
+                myLogger.logit(.info, message: "Context Item \(item): \(String(describing: result))")
             }
         }
         
         for item in hintKeys {
-            var result = getHint(hintType: item)
+            let result = getHint(hintType: item)
             if result != nil {
-                NSLog("Hint Item \(item): \(result)")
-            } else {
-                result = getContext(contextType: item)
-                NSLog("Context Item \(item): \(result)")
+                myLogger.logit(.info, message: "Hint Item \(item): \(String(describing: result))")
             }
         }
-        
-        allowLogin()
+        let _ = allowLogin()
     }
 }
