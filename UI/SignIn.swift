4a7452190053a6c9813d4984db0bcf24a3cd9900
Josh Wisenbaker <josh@orchardandgrove.com>
2017-12-07 12:44:21 -0500

    Refactoring in SingIn.swift. Consolidated hint and context setting methods.

--- UI/SignIn.swift
+++ UI/SignIn.swift
@@ -18,15 +18,7 @@ class SignIn: NSWindowController {
     var mech: MechanismRecord?
     var session: NoMADSession?
     
-    //MARK: - setup statics
-    
-    let kNoMADUser = "NoMAD.user"
-    let kNoMADPass = "NoMAD.pass"
-    let kNoMADFirst = "NoMAD.first"
-    let kNoMADLast = "NoMAD.last"
-    
     //MARK: - IB outlets
-    
     @IBOutlet weak var username: NSTextField!
     @IBOutlet weak var password: NSSecureTextField!
     @IBOutlet weak var domain: NSPopUpButton!
@@ -34,23 +26,13 @@ class SignIn: NSWindowController {
     @IBOutlet weak var spinner: NSProgressIndicator!
     @IBOutlet weak var imageView: NSImageView!
     
-    //override var windowNibName: NSNib.Name? = NSNib.Name(rawValue: "SignIn")
-    
+
     //MARK: - UI Methods
     override func windowDidLoad() {
         super.windowDidLoad()
-        
-        // get an image
-        let fameworkURL = Bundle.init(identifier: "menu.nomad.NoMADLoginAD")?.bundleURL
-        
-        NSLog((fameworkURL?.absoluteString)!)
-        
-        //imageView.image = logoImage
-        
         self.window?.isMovable = false
         self.window?.canBecomeVisibleWithoutLogin = true
         self.window?.level = NSWindow.Level(rawValue: NSWindow.Level.screenSaver.rawValue + 1)
-        //self.window?.makeKeyAndOrderFront(self)
         self.window?.orderFrontRegardless()
         
         // make things look better
@@ -62,35 +44,22 @@ class SignIn: NSWindowController {
     @IBAction func signInClick(_ sender: Any) {
         animateUI()
         if NoLoMechanism.checkForLocalUser(name: username.stringValue) {
-            NSLog("User exists... passing on")
             setHints()
             completeLogin(authResult: .allow)
-        } else if username.stringValue == "eject" {
-            // need to eject
-            NSLog("ejecting")
-            completeLogin(authResult: .allow)
-        } else if username.stringValue == "" {
+        }  else if username.stringValue == "" {
             // nothing to do here
         } else {
-            NSLog("%@", "Lets try to login...")
-
-            
-            NSLog("%@", "Start a NoMAD session...")
             session = NoMADSession.init(domain: (domain.selectedItem?.title.uppercased())!, user: username.stringValue)
-            
-            if session == nil {
-                NSLog("%@", "Session was not created")
+            guard let session = session else {
+                NSLog("%@", "Could not create NoMADSession")
+                return
             }
-            
-            NSLog("%@", "Set the session pass...")
-            session?.userPass = password.stringValue
-            session?.delegate = self
-            
-            NSLog("%@", "Try to authenticate...")
-            session?.authenticate()
+            session.userPass = password.stringValue
+            session.delegate = self
+            session.authenticate()
         }
     }
-    
+
     /// Simple toggle to change the state of the NoLo window UI between active and inactive.
     func animateUI() {
         signIn.isEnabled = !signIn.isEnabled
@@ -103,17 +72,47 @@ class SignIn: NSWindowController {
         spinner.isHidden ? spinner.stopAnimation(nil) : spinner.startAnimation(nil)
     }
 
-    //MARK: - User Utility Functions
+    //MARK: - Login Context Functions
 
     /// Set the authorization and context hints.
     fileprivate func setHints() {
-        setPassHint(user: password.stringValue)
-        setUserHint(user: username.stringValue)
+        setHint(type: .user, hint: username.stringValue)
+        setHint(type: .pass, hint: password.stringValue)
 
-        setPassContext(pass: password.stringValue)
-        setUserContext(user: username.stringValue)
+        setContext(type: kAuthorizationEnvironmentUsername, value: username.stringValue)
+        setContext(type: kAuthorizationEnvironmentPassword, value: password.stringValue)
     }
 
+    /// Set a NoMAD Login Authorization mechanism hint.
+    ///
+    /// - Parameters:
+    ///   - type: A value from `HintType` representing the NoMad Login value to set.
+    ///   - hint: A `String` of the hint value to set.
+    func setHint(type: HintType, hint: String) {
+        let data = NSKeyedArchiver.archivedData(withRootObject: hint)
+        var value = AuthorizationValue(length: data.count, data: UnsafeMutableRawPointer(mutating: (data as NSData).bytes.bindMemory(to: Void.self, capacity: data.count)))
+        let err = (mech?.fPlugin.pointee.fCallbacks.pointee.SetHintValue((mech?.fEngine)!, type.rawValue, &value))!
+        guard err == errSecSuccess else {
+            NSLog("Set hint failed with: %@", err)
+            return
+        }
+    }
+
+    /// Set one of the known `AuthorizationTags` values to be used during mechanism evaluation.
+    ///
+    /// - Parameters:
+    ///   - type: A `String` constant from AuthorizationTags.h representing the value to set.
+    ///   - value: A `String` value of the context value to set.
+    func setContext(type: String, value: String) {
+        let tempdata = value + "\0"
+        let data = tempdata.data(using: .utf8)
+        var value = AuthorizationValue(length: (data?.count)!, data: UnsafeMutableRawPointer(mutating: (data! as NSData).bytes.bindMemory(to: Void.self, capacity: (data?.count)!)))
+        let err = (mech?.fPlugin.pointee.fCallbacks.pointee.SetContextValue((mech?.fEngine)!, type, .extractable, &value))!
+        guard err == errSecSuccess else {
+            NSLog("Set context value failed with: %@", err)
+            return
+        }
+    }
 
     /// Complete the NoLo process and either continue to the next Authorization Plugin or reset the NoLo window.
     ///
@@ -123,75 +122,6 @@ class SignIn: NSWindowController {
         animateUI()
         NSApp.abortModal()
         self.window?.close()
-    }
-
-    func setPassHint(user: String) -> Bool {
-        NSLog("Setting passhint: %@ %i", #file, #line)
-        guard let data : Data = NSKeyedArchiver.archivedData(withRootObject: user)
-            else {
-                NSLog("NoLo:Failed to update user pass in hint.");
-                return false
-        }
-        var value = AuthorizationValue(length: data.count, data: UnsafeMutableRawPointer(mutating: (data as NSData).bytes.bindMemory(to: Void.self, capacity: data.count)))
-        let err = (mech?.fPlugin.pointee.fCallbacks.pointee.SetHintValue((mech?.fEngine)!, kNoMADPass, &value))!
-        return (err == errSecSuccess)
-    }
-    
-    func setUserHint(user: String) -> Bool {
-        NSLog("Setting user: %@ %i", #file, #line)
-        guard let data : Data = NSKeyedArchiver.archivedData(withRootObject: user)
-            else {
-                NSLog("NoLo:Failed to update user name in hint.");
-                return false
-        }
-        var value = AuthorizationValue(length: data.count, data: UnsafeMutableRawPointer(mutating: (data as NSData).bytes.bindMemory(to: Void.self, capacity: data.count)))
-        let err : OSStatus = (mech?.fPlugin.pointee.fCallbacks.pointee.SetHintValue((mech?.fEngine)!, kNoMADUser, &value))!
-        return (err == errSecSuccess)
-    }
-    
-    func setPassContext(pass: String) -> Bool {
-        
-        // silly two-step
-        let flags = AuthorizationContextFlags.extractable
-//        let flags = AuthorizationContextFlags(rawValue: AuthorizationContextFlags.RawValue(1 << 0))
-
-        // add null byte to signify end of string
-        
-        let tempdata = pass + "\0"
-        var data = tempdata.data(using: .utf8)
-        
-        //var value = AuthorizationValue(length: (data?.count)!, data: &data)
-        
-        var value = AuthorizationValue(length: (data?.count)!,
-                                       data: UnsafeMutableRawPointer(mutating: (data as! NSData).bytes.bindMemory(to: Void.self, capacity: (data?.count)!)))
-        
-        let err = mech?.fPlugin.pointee.fCallbacks.pointee.SetContextValue((mech?.fEngine)!, kAuthorizationEnvironmentPassword, flags, &value)
-        
-        NSLog("Setting pass context")
-        
-        return (err == errSecSuccess)
-    }
-
-    //TODO: # unify context settings
-    func setUserContext(user: String) -> Bool {
-        
-        // silly two-step
-        
-        // add null byte to signify end of string
-        
-        let tempdata = user + "\0"
-        var data = tempdata.data(using: .utf8)
-        let flags = AuthorizationContextFlags.extractable
-//        let flags = AuthorizationContextFlags(rawValue: AuthorizationContextFlags.RawValue(1 << 0))
-
-        var value = AuthorizationValue(length: (data?.count)!,
-                                       data: UnsafeMutableRawPointer(mutating: (data! as NSData).bytes.bindMemory(to: Void.self, capacity: (data?.count)!)))
-        
-        let err = mech?.fPlugin.pointee.fCallbacks.pointee.SetContextValue((mech?.fEngine)!, kAuthorizationEnvironmentUsername, flags, &value)
-        
-        NSLog("Setting user context")
-        
-        return (err == errSecSuccess)
     }
 }
 
@@ -210,8 +140,6 @@ extension SignIn: NoMADUserSessionDelegate {
     func NoMADUserInformation(user: ADUserRecord) {
         NSLog("Looking up info for: %@", user.shortName)
         setHints()
-        // user information lookup
-
         completeLogin(authResult: .allow)
     }
 }
