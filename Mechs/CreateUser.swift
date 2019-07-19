bc796fcf58df2e18f1e528e8fa5e76e5b77dfbbb
Johan McGwire <Johan@McGwire.tech>
2019-07-19 10:58:49 -0400

    Added UseCNForFullNameFallback

--- Mechs/CreateUser.swift
+++ Mechs/CreateUser.swift
@@ -139,7 +139,10 @@ class CreateUser: NoLoMechanism {
         
         if getManagedPreference(key: .UseCNForFullName) as? Bool ?? false {
             attrs[kODAttributeTypeFullName] = [getHint(type: .noMADFull) as? String ?? ""]
+        } else if getManagedPreference(key: .UseCNForFullNameFallback) as? Bool ?? false && getHint(type: .noMADFull) as? String ?? " " == " " {
+            attrs[kODAttributeTypeFullName] = [getHint(type: .noMADFull) as? String ?? ""]
         }
+        
         
         if let signInTime = getHint(type: .networkSignIn) {
             attrs[kODAttributeNetworkSignIn] = [signInTime]
