f2893e1f2b21e6e80e9dc4ea56940d63054efce3
Joel Rennich <joel.rennich@jamf.com>
2020-03-27 14:06:41 -0500

    add default guest user

--- UI/SignIn.swift
+++ UI/SignIn.swift
@@ -317,20 +317,19 @@ class SignIn: NSWindowController {
             
             os_log("Checking for guest account", log: uiLog, type: .default)
             
-            if let guestUsers = getManagedPreference(key: .GuestUserAccounts) as? [String] {
-                if guestUsers.contains(username.stringValue) {
-                    os_log("Guest user engaging", log: uiLog, type: .default)
-                    setHint(type: .guestUser, hint: "true")
-                    shortName = username.stringValue
-                    passString = UUID.init().uuidString
-                    setHint(type: .noMADDomain, hint: "GUEST")
-                    setHint(type: .noMADFirst, hint: getManagedPreference(key: .GuestUserFirst) as? String ?? "Guest")
-                    setHint(type: .noMADLast, hint: getManagedPreference(key: .GuestUserLast) as? String ?? "User")
-                    setHint(type: .noMADFull, hint: (getManagedPreference(key: .GuestUserFirst) as? String ?? "Guest") + (getManagedPreference(key: .GuestUserLast) as? String ?? "User"))
-                    setRequiredHintsAndContext()
-                    completeLogin(authResult: .allow)
-                    return
-                }
+            let guestUsers = getManagedPreference(key: .GuestUserAccounts) as? [String] ?? ["guest"]
+            if guestUsers.contains(username.stringValue) {
+                os_log("Guest user engaging", log: uiLog, type: .default)
+                setHint(type: .guestUser, hint: "true")
+                shortName = username.stringValue
+                passString = UUID.init().uuidString
+                setHint(type: .noMADDomain, hint: "GUEST")
+                setHint(type: .noMADFirst, hint: getManagedPreference(key: .GuestUserFirst) as? String ?? "Guest")
+                setHint(type: .noMADLast, hint: getManagedPreference(key: .GuestUserLast) as? String ?? "User")
+                setHint(type: .noMADFull, hint: (getManagedPreference(key: .GuestUserFirst) as? String ?? "Guest") + (getManagedPreference(key: .GuestUserLast) as? String ?? "User"))
+                setRequiredHintsAndContext()
+                completeLogin(authResult: .allow)
+                return
             }
         }
         
